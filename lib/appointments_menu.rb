a88ab162fb07e428c7dcdf8905600b2b0c803b7d
Guy Halperin & Austin Steward <student@epicodus.com>
2014-08-07 11:32:59 -0700

    finish ui

--- lib/appointments_menu.rb
+++ lib/appointments_menu.rb
@@ -3,12 +3,15 @@ def appointment_menu
   puts "Enter 'a' to add an appointment"
   puts "Enter 'l' to list appointments"
   puts "Enter 'm' to return to main menu"
+  puts "Enter 's' to see the billed sum of a doctor"
   user_choice = gets.chomp.downcase
   case(user_choice)
     when 'a'
       add_appointment
     when 'l'
       list_appointments
+    when 's'
+      billed_sum
     when 'm'
       main_menu
     else
@@ -37,4 +40,14 @@ def list_appointments
   Appointment.all.each do |appointment|
     puts "Doctor: #{appointment.doctor} Patient: #{appointment.patient} Date: #{appointment.date} Cost: #{appointment.cost}"
   end
+end
+
+def billed_sum
+  puts "Enter the doctor's name"
+  doctor = gets.chomp.upcase
+  puts "Enter the begin date"
+  start_date = gets.chomp
+  puts "Enter the end date"
+  end_date = gets.chomp
+  puts "Amount Billed:  " + Appointment.search_date(doctor, start_date, end_date).to_s
 end
